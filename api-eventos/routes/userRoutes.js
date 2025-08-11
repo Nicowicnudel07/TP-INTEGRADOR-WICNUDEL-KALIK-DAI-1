a661040a60707b53c304d30b001d22fbbad1656c
Frantag15 <48183956@ort>
2025-08-11 12:05:30 -0300

    polshutequiero

--- api-eventos/routes/userRoutes.js
+++ api-eventos/routes/userRoutes.js
@@ -6,14 +6,3 @@ router.post('/register', userController.register);
 router.post('/login', userController.login);
 
 module.exports = router;
-
-/*
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/user.controller');
-
-router.post('/register', userController.register);
-router.post('/login', userController.login);
-
-module.exports = router;
-*/
